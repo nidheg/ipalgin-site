947fdda62be457f499d058f9a36c3536ec163b1a
nidheg <maxim-algin@mail.ru>
2012-01-25 15:04:37 +0600

    Initital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ipalgin::Application.config.secret_token = 'bfb2b2e0b0ab910d7766ab13a8f26e0655ea839d51d0edf20165f9ba1952c2a488f0b6fd5126f0cc31b5d0056ecb99c212a2b603fd1afb8bd96be261e42db6f5'
